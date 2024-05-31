e05e018168ddc7cce76d4ace75a27119f7344da7
aymane239 <2393372@bdeb.qc.ca>
2024-05-30 23:33:56 -0400

    Ajout de la requête SQL pour récupérer les ingrédients par recette

--- js_page.js
+++ js_page.js
@@ -9,3 +9,12 @@ async function getIngredientsByRecipe(recipeId) {
         password: 'oracle',
         connectString: 'localhost/XE'
       });
+
+      const result = await connection.execute(`
+      SELECT i.nom_ingredient, ri.quantite, ri.udm
+      FROM ingredient i
+      JOIN recette_ingredient ri ON i.id_ingredient = ri.id_ingredient
+      WHERE ri.id_recette = :recipeId
+    `, [recipeId]);
+
+    console.log(result.rows);
