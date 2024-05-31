b90f3712202e3706b432b6aad6e023b04b1382a7
aymane239 <2393372@bdeb.qc.ca>
2024-05-30 23:17:06 -0400

    Ajout de la fonction pour récupérer les utilisateurs via l'API

--- inscription-1.js
+++ inscription-1.js
@@ -5,3 +5,21 @@ function createNode(element) {
   function append(parent, el) {
     return parent.appendChild(el);
   }
+
+  function fetchUsers() {
+    const url = "http://localhost:8080/ords/hr2/inscription/";
+    
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const users = data.items;
+        console.log(...users.map(user => user.id_utilisateur));
+        const maxId = Math.max(...users.map(user => user.id_utilisateur));
+        const newId = maxId + 1;
+
+        saveData(newId);
+      })
+      .catch(error => {
+        console.error('Erreur lors de la récupération des utilisateurs :', error);
+      });
+  }
