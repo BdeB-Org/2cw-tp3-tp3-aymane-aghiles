177c7c0a7c9d1ebd8950080cc9d9350b71480b94
benallek aghiles <2253929@bdeb.qc.ca>
2024-05-30 23:06:45 -0400

    ajout du code js dans ingredient.js

---
+++ ingredient.js
@@ -0,0 +1,45 @@
+function createNode(element){
+
+    return document.createElement(element);
+   
+   }
+   
+   function append(parent, el){
+   
+   return parent.appendChild(el); //el = element
+   
+   }
+   
+ 
+   
+   const url = "http://localhost:8080/ords/hr2/ingredient";
+   
+   fetch(url)
+   
+   .then((resp) => resp.json())
+   
+   .then(function (data) {
+   console.log(data);
+   let employees = data.items;
+   
+   return employees.map (function(ingredient) {
+   
+   let li = createNode("li"),
+   
+  span = createNode("span");
+   
+   span.innerHTML = `${ingredient.nom_ingredient}`;
+   
+  append(li,span);
+   
+  append(document.getElementById('ingredientsList'), li);
+   
+    });
+   
+   })
+   
+   .catch(function(error) {
+   
+   console.log(error);
+   
+   });
